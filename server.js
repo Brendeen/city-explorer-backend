3444ede2aadf336bbb219a44786c799d6198ade1
Brenden Moore <brendenm3603@gmail.com>
2023-02-04 20:09:37 -0800

    modified server as best I could to modualize, but not working yet

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3002;
 
+const movies = require('./library/movies')
+
 
 const weatherData = require('./data/weather.json');
+const { send } = require('process');
 
 
 app.get('/', (request, response) => {
@@ -33,60 +36,34 @@ app.get('/bruh', (req, res) => {
 });
 
 app.get('/weather', async (req, res) => {
-  // let searchQuery = req.query.searchQuery;
-  let lat = req.query.lat
-  let lon = req.query.lon
-  // let foundCity = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase()) - old static json data
-  let weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_KEY}&lat=${lat}&lon=${lon}&days=10`
-  console.log(weatherUrl)
-  try{
-  let response = await axios.get(weatherUrl);
-    console.log(response.data.data)
-  let weatherArr = response.data.data.map(obj => new Forecast(obj))
-
-  res.status(201).send(weatherArr);
-  }catch(error){
-    res.status(500).send('Error has accured')
-  }
-});
-
-app.get('/movies', async (req, res) => {
-  let query = req.query.query
-
-  let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEDB_KEY}&query=${query}`
-
-  try{
-    let response2 = await axios.get(movieUrl);
-    
-    let movieArr = response2.data.results.map(obj => new Movies(obj))
-
-    res.status(200).send(movieArr.slice(0, 5))
-  }catch(error){
-    res.status(500).send('Error has accured')
-  }  
+  // let lat = req.query.lat
+  // let lon = req.query.lon
+  // let weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_KEY}&lat=${lat}&lon=${lon}&days=10`
   
+  // Old JSON static data
+  // let searchQuery = req.query.searchQuery;
+  // let foundCity = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase()) - old static json data
+
+  // try{
+  // let response = await axios.get(weatherUrl);
+  //   console.log(response.data.data)
+  // let weatherArr = response.data.data.map(obj => new Forecast(obj))
+
+  // res.status(201).send(weatherArr);
+  // }catch(error){
+  //   res.status(500).send('Error has accured')
+  // }
+
+  axios.get(weatherArr)
+    .then(data => {
+      let formatedWeather = data.data.data.map(weaObj => new Forecast(weaObj))
+      res.status(200).send(formatedWeather)
+    })
+    .catch(error => send('Error has accured'));
+
 });
 
-class Forecast {
-  constructor(day) {
-    this.date = day.valid_date
-    this.description = day.weather.description
-  }
+app.get('/movies', movies);
 
-}
-
-class Movies {
-  constructor(mov) {
-    this.title = mov.title
-    this.overview = mov.overview
-    this.average_votes = mov.vote_average
-    this.total_votes = mov.vote_count
-    this.image_url = mov.poster_path
-    this.popularity = mov.popularity
-    this.released_on = mov.release_date
-    
-
-  }
-}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
